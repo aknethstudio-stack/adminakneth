24933859d93dd31a94745d992f62474019bb7942
Katarzyna Pawłowska-Malesa (Akneth Studio) <akneth.studio@gmail.com>
2025-08-24 05:46:33 +0200

    feat: config jest

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+![CI Status](https://github.com/aknethstudio-stack/adminakneth/actions/workflows/ci.yml/badge.svg)
+
 # adminpanel
---
+++ jest.config.mjs
@@ -0,0 +1,26 @@
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/app/(.*)$': '<rootDir>/src/app/$1',
+    '^@/styles/(.*)$': '<rootDir>/src/styles/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/app/layout.tsx',
+    '!src/app/globals.scss',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+}
+
+export default createJestConfig(customJestConfig)
